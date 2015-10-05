53aa586ba00d80fc0d9800284312a6b5fdfba17c
Giorgi Shvelidze <giorgishvelidze@mia.gov.ge>
2015-10-05 15:12:44 +0400

    improved tests

--- src/Source.cpp
+++ src/Source.cpp
@@ -13,132 +13,156 @@ void balancingExample();
 int calculateHeight(Node<int>*);
 inline int Max(int, int);
 
-void balancing_rbt_via_balanceDSW(int);
-void balancing_rbt_via_balanceR(int);
-void balancing_rbt_via_balanceMod(int);
+double balancing_rbt_via_balanceDSW(int, bool);
+double balancing_rbt_via_balanceR(int, bool);
+double balancing_rbt_via_balanceMod(int, bool);
 
 double getAverage(double *arr, int len);
-double building_rbt_random(int);
-double building_rbt_inorder(int);
+double building_rbt_random(int, bool);
+double building_rbt_inorder(int, bool);
 
-void balancing_rbt_via_balanceDSW(int tree_size) {
+double balancing_rbt_via_balanceDSW(int tree_size, bool print) {
 	if (!tree_size || tree_size > MAX_TREE_SIZE) {
 		cout << "Invalid tree size" << endl;
-		return;
+		return 0;
 	}
-	cout << "Balancing Red-black tree with " << tree_size
-			<< " nodes via \"DSW\" method" << endl;
+	if (print) cout << "Balancing Red-black tree with " << tree_size << " nodes via \"DSW\" method" << endl;
 
 	Tree<int> tree;
 	clock_t start, finish;
 
-	cout << "Test started..." << endl << endl;
+	if (print) cout << "Test started..." << endl << endl;
 
 	for (int i = 0; i < tree_size; i++) {
 		Node<int>* node = new Node<int>(rand() % MAX_KEY_VALUE);
 		tree.insert(node);
 	}
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl;
+	if (print) {
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl;
 
-	cout << "balancing..." << endl;
+		cout << "balancing..." << endl;
+	}
 	start = clock();
 	tree.balanceDSW();
 	finish = clock();
-	cout << "balanced in: " << ((double) (finish - start)) / 1000 << " ms"
-			<< endl;
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+	double time = ((double) (finish - start)) / 1000;
 
-	cout << "Test completed successfully!" << endl << endl;
+	if (print) {
+		cout << "balanced in: " << time << " ms" << endl;
+
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+
+		cout << "Test completed successfully!" << endl << endl;
+	}
+
+	return time;
 }
 
-void balancing_rbt_via_balanceR(int tree_size) {
+double balancing_rbt_via_balanceR(int tree_size, bool print) {
 	if (!tree_size || tree_size > MAX_TREE_SIZE) {
 		cout << "Invalid tree size" << endl;
-		return;
+		return 0;
 	}
-	cout << "Balancing Red-black tree with " << tree_size
-			<< " nodes via \"balanceR\" method" << endl;
+	if (print) cout << "Balancing Red-black tree with " << tree_size << " nodes via \"balanceR\" method" << endl;
 
 	Tree<int> tree;
 	clock_t start, finish;
 
-	cout << "Test started..." << endl << endl;
+	if (print) cout << "Test started..." << endl << endl;
 
 	for (int i = 0; i < tree_size; i++) {
 		Node<int>* node = new Node<int>(rand() % MAX_KEY_VALUE);
 		tree.insert(node);
 	}
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl;
+	if (print) {
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl;
 
-	cout << "balancing..." << endl;
+		cout << "balancing..." << endl;
+	}
+
 	start = clock();
 	tree.setModeOST();
 	tree.balance();
 	tree.setModeRBT();
 	finish = clock();
-	cout << "balanced in: " << ((double) (finish - start)) / 1000 << " ms"
-			<< endl;
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+	double time = ((double) (finish - start)) / 1000;
 
-	cout << "Test completed successfully!" << endl << endl;
+	if (print) {
+		cout << "balanced in: " << time << " ms" << endl;
+
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+
+		cout << "Test completed successfully!" << endl << endl;
+	}
+
+	return time;
 }
 
-void balancing_rbt_via_balanceMod(int tree_size) {
+double balancing_rbt_via_balanceMod(int tree_size, bool print) {
 	if (!tree_size || tree_size > MAX_TREE_SIZE) {
 		cout << "Invalid tree size" << endl;
-		return;
+		return 0;
 	}
-	cout << "Balancing Red-black tree with " << tree_size
-			<< " nodes via \"balanceMod\" method" << endl;
+
+	if (print) cout << "Balancing Red-black tree with " << tree_size << " nodes via \"balanceMod\" method" << endl;
 
 	Tree<int> tree;
 	clock_t start, finish;
 
-	cout << "Test started..." << endl << endl;
+	if (print) cout << "Test started..." << endl << endl;
 
 	for (int i = 0; i < tree_size; i++) {
 		Node<int>* node = new Node<int>(rand() % MAX_KEY_VALUE);
 		tree.insert(node);
 	}
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl;
 
-	cout << "balancing..." << endl;
+	if (print) {
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl;
+
+		cout << "balancing..." << endl;
+	}
+
 	start = clock();
 	tree.setModeOST();
 	tree.balanceMod();
 	tree.setModeRBT();
 	finish = clock();
-	cout << "balanced in: " << ((double) (finish - start)) / 1000 << " ms"
-			<< endl;
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+	double time = ((double) (finish - start)) / 1000;
 
-	cout << "Test completed successfully!" << endl << endl;
+	if (print) {
+		cout << "balanced in: " << time << " ms" << endl;
+
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+
+		cout << "Test completed successfully!" << endl << endl;
+	}
+
+	return time;
 }
 
-double building_rbt_random(int tree_size) {
+double building_rbt_random(int tree_size, bool print) {
 	if (!tree_size || tree_size > MAX_TREE_SIZE) {
 		cout << "Invalid tree size" << endl;
 		return 0;
 	}
-	cout << "Building Red-black tree with " << tree_size << " nodes. Randomly"
-			<< endl;
+	if (print) cout << "Building Red-black tree with " << tree_size << " nodes. Randomly" << endl;
 
 	Tree<int> tree;
 	clock_t start, finish;
 
-	cout << "started..." << endl;
+	if (print) cout << "started..." << endl;
 	start = clock();
 	for (int i = 0; i < tree_size; i++) {
 		Node<int>* node = new Node<int>(rand() % MAX_KEY_VALUE);
@@ -146,28 +170,29 @@ double building_rbt_random(int tree_size) {
 	}
 	finish = clock();
 	double time = ((double) (finish - start)) / 1000;
-	cout << "finished in: " << time << " ms" << endl;
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
-
-	cout << "Test completed successfully!" << endl << endl;
+	if (print) {
+		cout << "finished in: " << time << " ms" << endl;
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+		cout << "Test completed successfully!" << endl << endl;
+	}
 
 	return time;
 }
 
-double building_rbt_inorder(int tree_size) {
+double building_rbt_inorder(int tree_size, bool print) {
 	if (!tree_size || tree_size > MAX_TREE_SIZE) {
 		cout << "Invalid tree size" << endl;
 		return 0;
 	}
-	cout << "Building Red-black tree with " << tree_size << " nodes. Inorder"
-			<< endl;
+	if (print) cout << "Building Red-black tree with " << tree_size << " nodes. Inorder" << endl;
 
 	Tree<int> tree;
 	clock_t start, finish;
 
-	cout << "started..." << endl;
+	if (print) cout << "started..." << endl;
+
 	start = clock();
 	for (int i = 0; i < tree_size; i++) {
 		Node<int>* node = new Node<int>(i);
@@ -175,12 +200,15 @@ double building_rbt_inorder(int tree_size) {
 	}
 	finish = clock();
 	double time = ((double) (finish - start)) / 1000;
-	cout << "finished in: " << time << " ms" << endl;
 
-	cout << "tree size: " << tree.getSize();
-	cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+	if (print) {
+		cout << "finished in: " << time << " ms" << endl;
 
-	cout << "Test completed successfully!" << endl << endl;
+		cout << "tree size: " << tree.getSize();
+		cout << "; tree height: " << calculateHeight(tree.root) << endl << endl;
+
+		cout << "Test completed successfully!" << endl << endl;
+	}
 
 	return time;
 }
@@ -188,20 +216,20 @@ double building_rbt_inorder(int tree_size) {
 int main() {
 	srand(time(NULL));
 
-	int n = 10;
-	double times[n];
-
-	for (int i = 1000; i <= MAX_TREE_SIZE; i *= 10) {
+	for (int i = 1000; i <= 100000; i *= 10) {
+		int n = 10;
+		double times[n];
 		for (int j = 0; j < n; j++) {
-			times[j] = building_rbt_random(i);
+			times[j] = building_rbt_random(i, false);
 		}
+		cout << "Average time: " << getAverage(times, n) << endl << endl;
 
-//		building_rbt_inorder(i);
-
-//		balancing_rbt_via_balanceR(i);
-//		balancing_rbt_via_balanceMod(i);
-//		balancing_rbt_via_balanceDSW(i);
+		building_rbt_inorder(i, false);
+		balancing_rbt_via_balanceR(i, false);
+		balancing_rbt_via_balanceMod(i, false);
+		balancing_rbt_via_balanceDSW(i, false);
 	}
+
 }
 
 double getAverage (double *arr, int len) {
